a4b7134b14d9748b6e035bb5685513419f68f041
michelegelmini <michelegelminivr@gmail.com>
2024-05-20 16:23:05 +0200

    add: pari-dispari-verifica

--- pari-e-dispari/main.js
+++ pari-e-dispari/main.js
@@ -1,1 +1,20 @@
 'use strict'
+
+//prompt per l'utente
+let userChoice = prompt('Pari o dispari?');
+let userChoiceFormatted = userChoice.toLowerCase();
+
+//verifica della scelta pari o dispari
+switch (userChoiceFormatted) {
+    case "pari":
+        userChoice = 'pari'
+        break;
+    case "dispari":
+        userChoice = 'dispari'
+        break;
+    default:
+        userChoice = prompt('Inserisci un valore valido: pari o dispari?');
+        break;
+}
+
+const userNumber = Number(prompt('Scegli un numero da 1 a 5'));
