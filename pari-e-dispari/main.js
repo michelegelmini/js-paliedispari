f8c075c8a0f8287dcd36851ca1914c27568400e5
michelegelmini <michelegelminivr@gmail.com>
2024-05-20 16:43:15 +0200

    add: while-conditions

--- pari-e-dispari/main.js
+++ pari-e-dispari/main.js
@@ -1,20 +1,42 @@
 'use strict'
 
-//prompt per l'utente
+//prompt pari o dispari per l'utente
 let userChoice = prompt('Pari o dispari?');
 let userChoiceFormatted = userChoice.toLowerCase();
 
 //verifica della scelta pari o dispari
-switch (userChoiceFormatted) {
-    case "pari":
-        userChoice = 'pari'
-        break;
-    case "dispari":
-        userChoice = 'dispari'
-        break;
-    default:
-        userChoice = prompt('Inserisci un valore valido: pari o dispari?');
-        break;
+while (!(userChoiceFormatted == 'pari' || userChoiceFormatted == 'dispari')) {
+    userChoice = prompt('Inserisci un valore valido: pari o dispari?');
+    userChoiceFormatted = userChoice.toLowerCase();
+    console.log(userChoiceFormatted);
 }
 
-const userNumber = Number(prompt('Scegli un numero da 1 a 5'));
+//prompt numero utente
+let userNumber = Number(prompt('Scegli un numero da 1 a 5'));
+
+while (!(userNumber >= 1 && userNumber <= 5)) {
+    userNumber = prompt('Inserisci un valore valido');
+} console.log(`il tuo numero è ${userNumber}`);
+
+
+
+
+
+
+
+
+
+
+// switch (userChoiceFormatted) {
+//     case "pari":
+//         userChoice = 'pari'
+//         console.log(userChoice);
+//         break;
+//     case "dispari":
+//         userChoice = 'dispari'
+//         console.log(userChoice);
+//         break;
+//     default:
+//         userChoice = prompt('Inserisci un valore valido: pari o dispari?');
+//         break;
+// }
