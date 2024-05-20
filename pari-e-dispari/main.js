9ad7d4010f98f7b53ccbf496507a0050fe755985
michelegelmini <michelegelminivr@gmail.com>
2024-05-20 16:57:28 +0200

    add: who-won-function

--- pari-e-dispari/main.js
+++ pari-e-dispari/main.js
@@ -23,12 +23,27 @@ while (!(userNumber >= 1 && userNumber <= 5)) {
 function getRandomInt(max) {
     return Math.ceil(Math.random() * max);
 }
-
 const computerNumber = getRandomInt(5);
-console.log(computerNumber);
+console.log(`il numero del pc è ${computerNumber}`);
 
+// somma dei numeri
+const sum = userNumber + computerNumber
+console.log(`la somma è ${sum}`)
 
+const module = sum % 2;
 
+//verifica della vittoria
+function whoWon(odds, sumModule) {
+    if (odds === 'pari' && sumModule === 0) {
+        console.log('hai vinto!')
+    } else if (odds === 'dispari' && sumModule === 1) {
+        console.log('hai vinto!')
+    } else {
+        console.log('hai perso')
+    }
+}
+
+whoWon(userChoice, module);
 
 
 // switch (userChoiceFormatted) {
